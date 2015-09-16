78bbffa40627a50b3fb4159b4f8487c71359336f
Dan Skaggs <dan.skaggs@web-meister.com>
2015-09-15 21:15:07 -0400

    First pass at talking to the API

--- app/scripts/profile/profileCtrl.js
+++ app/scripts/profile/profileCtrl.js
@@ -1,12 +1,27 @@
 angular.module('tappr.profile', [])
 
-.controller('ProfileCtrl', ['$scope', function($scope) {
+.controller('ProfileCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.messages = [];
+    $scope.user = {};
 
     function init () {
         console.log('INIT');
         $scope.messages.push('ProfileCtrl is initted');
     }
+
+    $http({
+					method: 'GET',
+					url: '//localhost:8001/user/SharonDio',
+					params: params,
+					cache: useCached
+				})
+					.success(function (data) {
+						$scope.user = data;
+						console.log( data );
+					})
+					.error(function (data, status) {
+						console.log('OOPS!');
+					});
 
     init();
 }]);
