8f22ec95d31d5ac61271146e7d51f8f37750b47d
Sharon DiOrio <sdiorio-consultant@scholastic.com>
2015-09-20 13:15:36 -0400

    Star rating working.

--- app/scripts/beers/beerDetailCtrl.js
+++ app/scripts/beers/beerDetailCtrl.js
@@ -3,9 +3,12 @@ angular.module('tappr.beerdetail', [])
 .controller('BeerDetailCtrl', ['$scope', '$rootScope', '$routeParams', '$location', '$http',
         function($scope, $rootScope, $routeParams, $location, $http) {
     $scope.messages = [];
+    var updateStars;
 
     function init () {
         console.log('INIT', $routeParams);
+        $scope.starsNum = 5;
+
         if ($routeParams.id) {
             $http({
                 method: 'GET',
@@ -21,9 +24,36 @@ angular.module('tappr.beerdetail', [])
         } else {
             $location.url('/');
         }
+
+        //TODO Need rating value from profile if it exists
+        $scope.ratingValue = 0;
+
+        updateStars = function () {
+            $scope.stars = [];
+            for (var i = 0; i < $scope.starsNum; i++) {
+                $scope.stars.push({
+                    filled: i < $scope.ratingValue
+                });
+            }
+        };
+
+        updateStars();
     }
 
     init();
+
+    $scope.toggle = function(index) {
+        console.log('Toggle Rating: ', index);
+        if ($scope.readonly == undefined || $scope.readonly === false){
+            $scope.ratingValue = index + 1;
+            //TODO: Save Rating
+        }
+    };
+    $scope.$watch('ratingValue', function(oldValue, newValue) {
+        if (newValue) {
+            updateStars();
+        }
+    });
 
     $rootScope.$on('search', function (event, data) {
         "use strict";
