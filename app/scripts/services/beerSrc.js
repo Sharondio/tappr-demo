a1d3aedcc86582968ad2832e73b841b8d856b8c5
Dan Skaggs <dan.skaggs@web-meister.com>
2015-09-22 21:40:16 -0400

    Refactored beer controller to not directly use $http to load data but to call the beer service instead

--- app/scripts/services/beerSrc.js
+++ app/scripts/services/beerSrc.js
@@ -2,28 +2,58 @@ angular.module('tappr.services', []).
     factory('beerSvc', function($http) {
 
         var service = {};
-        var url = '//localhost:8001/beer';
+        var url = '//localhost:8001';
 
         service.find = function (queryTerm) {
-            return $http.get(url + '?q=' + queryTerm)
-                .success(function (data) {
-                    return data;
+
+            var params = {};
+            if(queryTerm){
+                params.q = queryTerm;
+            }
+            return $http({
+                method: 'GET',
+                url: url + '/beer',
+                params: params
+            })
+                .success(function (result) {
+                    return result;
                 })
-                .error( function (error) {
+                .error(function (data, status) {
                     console.log('ERROR: beerSrv: find: ', error);
-                    return;
+                    return status;
                 });
+
         };
 
         service.findOne = function (id) {
-            return $http.get(url + '/' + id)
-                .success(function (data) {
-                    return data;
+
+            return $http({
+                method: 'GET',
+                url: url + '/beer/' + id
+            })
+                .success(function (result) {
+                    return result;
                 })
-                .error( function (error) {
-                    console.log('ERROR: beerSrv: find: ', error);
-                    return;
+                .error(function (data, status) {
+                    console.log('ERROR: beerSrv: findOne: ', error);
+                    return status;
                 });
+
+        };
+
+        service.listCategories = function () {
+
+            return $http({
+                method: 'GET',
+                url: url + '/category'
+            })
+                .success(function (result) {
+                    return result;
+                })
+                .error(function (data, status) {
+                    return status;
+                });
+
         };
 
         return service;
