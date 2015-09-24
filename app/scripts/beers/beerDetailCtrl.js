a453bdcfe9970ab893f5c6484606779d80d85bda
Sharon DiOrio <sdiorio-consultant@scholastic.com>
2015-09-23 23:30:52 -0400

    Commit it while it's working!

--- app/scripts/beers/beerDetailCtrl.js
+++ app/scripts/beers/beerDetailCtrl.js
@@ -6,6 +6,7 @@ BeerDetailCtrl.$inject = ['$scope', '$rootScope', '$routeParams', '$location', '
 function BeerDetailCtrl ($scope, $rootScope, $routeParams, $location, userSrc, beerSrc) {
 
     var user = $rootScope.user.username;
+    $scope.ratingValue = undefined;
 
     function init () {
         console.log('INIT', $routeParams);
@@ -27,29 +28,47 @@ function BeerDetailCtrl ($scope, $rootScope, $routeParams, $location, userSrc, b
     }
 
     function getRating (beer) {
-        return userSrc.getRating(user, beer).then(ratingHandler, errorHandler);
+        return userSrc.getRating(user, beer).then(ratingHandler, ratingErrorHandler);
     }
 
     function ratingHandler (results) {
-        $scope.ratingValue = 0;
         if (results) {
             $scope.ratingValue = results.rating;
         }
     }
 
+    function ratingErrorHandler (error) {
+        console.log('ratingErrorHanlder: ', error);
+        if(error.status === 404) {
+            $scope.ratingValue = 0;
+            updateStars(true);
+        } else {
+            console.log('OOPS! Real error here. ', error);
+        }
+    }
+
     function getFavorite (beer) {
-        return userSrc.getFavorite(user, beer).then(getFavoriteHandler, errorHandler);
+        return userSrc.getFavorite(user, beer).then(getFavoriteHandler, getFavoriteErrorHandler);
     }
 
     function getFavoriteHandler (results) {
         $scope.isFavorite = results;
     }
 
+    function getFavoriteErrorHandler (error) {
+        if (error.status === 404) {
+            $scope.isFavorite = false;
+        } else {
+            console.log('OOPS! Real error here.');
+        }
+    }
+
     function errorHandler (error) {
-        console.log('OOPS!', error);
+        console.log('Controller OOPS!', error);
     }
 
     function updateStars (init) {
+        console.log('updating stars');
         $scope.stars = [];
         for (var i = 0; i < $scope.starsNum; i++) {
             $scope.stars.push({
