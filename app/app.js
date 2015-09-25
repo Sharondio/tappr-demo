1ce5feaa469634325b5d2ebb64f6448c03a18106
Sharon DiOrio <sdiorio-consultant@scholastic.com>
2015-09-25 10:30:07 -0400

    moved headerCtrl into its own file

--- app/app.js
+++ app/app.js
@@ -36,29 +36,3 @@ app.run(['$rootScope', '$location', '$cookieStore', 'configSrc', function ($root
 
 }]);
 
-app.controller('HeaderCtrl', function ($scope, $location, $rootScope, $cookieStore, $route) {
-    console.log('initing HeaderCtrl');
-
-    if ($cookieStore.get('login')) {
-        $scope.user = $cookieStore.get('login');
-        $rootScope.user = $cookieStore.get('login');
-    }
-
-    $scope.isActive = function (view) {
-        return (view === $location.path());
-    };
-
-    $scope.search = function () {
-        $rootScope.query = $scope.query;
-        $location.url('/beers/' + $scope.query);
-        $route.reload();
-    };
-
-    $scope.logout = function () {
-        $cookieStore.remove('login');
-        $scope.user = {};
-        $rootScope.user = {};
-        $route.reload();
-    };
-
-});
---
+++ app/scripts/home/headerCtrl.js
@@ -0,0 +1,27 @@
+angular.module('tappr.home')
+    .controller('HeaderCtrl', function ($scope, $location, $rootScope, $cookieStore, $route) {
+        console.log('initing HeaderCtrl');
+
+        if ($cookieStore.get('login')) {
+            $scope.user = $cookieStore.get('login');
+            $rootScope.user = $cookieStore.get('login');
+        }
+
+        $scope.isActive = function (view) {
+            return (view === $location.path());
+        };
+
+        $scope.search = function () {
+            $rootScope.query = $scope.query;
+            $location.url('/beers/' + $scope.query);
+            $route.reload();
+        };
+
+        $scope.logout = function () {
+            $cookieStore.remove('login');
+            $scope.user = {};
+            $rootScope.user = {};
+            $route.reload();
+        };
+
+    });
