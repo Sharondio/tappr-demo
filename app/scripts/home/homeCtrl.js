345d4b97a8375906ac5854465c27293414fce7c5
Sharon DiOrio <sdiorio-consultant@scholastic.com>
2015-09-26 13:39:35 -0400

    vm $watch

--- app/scripts/home/homeCtrl.js
+++ app/scripts/home/homeCtrl.js
@@ -1,12 +1,26 @@
 angular.module('tappr.home')
     .controller('HomeCtrl', HomeCtrl);
 
-HomeCtrl.$inject = ['$cookieStore', 'userSrc'];
+HomeCtrl.$inject = ['$scope', '$cookieStore', 'userSrc'];
 
-function HomeCtrl ($cookieStore, userSrc) {
+function HomeCtrl ($scope, $cookieStore, userSrc) {
 
     var vm = this;
     vm.user = userSrc.user;
+
+    // // $watch is a little tricky in controllerAs land.
+    // $scope.$watch(angular.bind(this, function () {
+    //     return this.username; // `this` IS the `this` above!!
+    // }), function (newVal, oldVal) {
+    //     // now we will pickup changes to newVal and oldVal
+    //     console.log('username changed!', newVal, oldVal);
+    // });
+
+    // John Papa says this should work. But it doesn't.
+    // $scope.$watch('vm.user.username', function(current, original) {
+    //     console.log('vm.user was %s', original);
+    //     console.log('vm.user is now %s', current);
+    // });
 
     vm.login = function() {
         userSrc.login(vm.username)
